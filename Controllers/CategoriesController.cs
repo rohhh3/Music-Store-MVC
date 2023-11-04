a066cf1c86181cc903041695dce1d76e5d406706
Krzysztof Pawłowski <82871@student.pb.edu.pl>
2023-11-04 20:16:49 +0100

    category view pages added navbar bug fix

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Accessories(){ return View(); }
+        public IActionResult Drums(){       return View(); }
+        public IActionResult GuitBass(){    return View(); }
+        public IActionResult Keys(){        return View(); }
+        public IActionResult Microphones(){ return View(); }
+        public IActionResult Software(){    return View(); }
+        public IActionResult Studio(){      return View(); }
+    }
+}
