b22784fa5705a5bd0fb816e7b5789aebf2f5ce9d
Krzysztof Pawlowski <krzysztofpeiv@o2.pl>
2023-11-15 18:29:01 +0100

    category model base

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicStore.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
---
+++ Models/MockCategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace MusicStore.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{ CategoryId = 1, Name = "Guitars/Basses" },
+                new Category{ CategoryId = 2, Name = "Keys" },
+                new Category{ CategoryId = 3, Name = "Drums" },
+                new Category{ CategoryId = 4, Name = "Studio" },
+                new Category{ CategoryId = 5, Name = "Microphones" },
+                new Category{ CategoryId = 6, Name = "Software" },
+                new Category{ CategoryId = 7, Name = "Accessories" },
+            };
+    }
+}
